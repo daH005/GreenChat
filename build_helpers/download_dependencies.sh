0ae8fbb57b5a27b5054d1c3759c6e9ff2f95e71c
dan005 <danil.shevelev.2004@mail.ru>
2024-10-26 15:09:30 +0500

    added new useful commands

--- build_helpers/download_dependencies.sh
+++ build_helpers/download_dependencies.sh
@@ -1,3 +1,6 @@
 sudo apt install python3-pip
 sudo apt install python3.11-dev default-libmysqlclient-dev build-essential
 python3.11 -m pip install virtualenv
+
+sudo apt install chromium-browser chromedriver
+sudo apt install mysql-server redis-server
