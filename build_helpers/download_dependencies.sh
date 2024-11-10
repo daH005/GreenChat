ab7e275c1ec5ed031ea5084b545b63989ad22b17
Данил <60618372+daH005@users.noreply.github.com>
2024-11-10 12:07:02 +0500

    [helpers] add

--- build_helpers/download_dependencies.sh
+++ build_helpers/download_dependencies.sh
@@ -1,10 +1,10 @@
 sudo apt install python3-pip
 sudo apt install python3.11-dev default-libmysqlclient-dev build-essential
-python3.11 -m pip install virtualenv
+sudo python3.11 -m pip install virtualenv
 
 sudo apt install chromium-browser
 sudo apt install chromium-chromedriver
 
 sudo apt install mysql-server redis-server
 
-npm install -g typescript
+sudo npm install -g typescript
